7b4222a7537e7aa2dad0156a65299071e31d4c3b
Leonvdk <leonvandeklundert@gmail.com>
2020-10-02 14:19:59 +0100

    searchDropdown options are now clickable but don't link yet. CSS has been made responsive and almost done

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ let currentInput = input.value;
 let htmlDropdown = document.querySelector('#searchDropdown');
 showSpots = [];
 
-const spots = ['Caiscais', 'Costa da Caparica', 'Nazaré', 'Belém', 'Cova do Vapor', 'Carcavelos', 'Bicas'];                 // Array with all spots
+let spots = ['Cascais', 'Costa da Caparica', 'Nazaré', 'Belém', 'Cova do Vapor', 'Carcavelos', 'Bicas'];                 // Array with all spots
 
 input.addEventListener("input", function dropdown(){
     
@@ -15,15 +15,13 @@ input.addEventListener("input", function dropdown(){
     for(let i = 0; i < spots.length; i++){
         if(spots[i].toLocaleLowerCase().includes(currentInput.toLocaleLowerCase())){
             htmlDropdown.style.visibility = "visible"                                // Display the updated list to the user
-            searchDropdown +=  "<hr>" + spots[i] 
+            searchDropdown +=  `<hr> <a href= "cascais.html">${spots[i]}</a>` 
             console.log("searchDropdown: ", searchDropdown)
             htmlDropdown.innerHTML = searchDropdown;}
         
         
         if(currentInput.length == 0 || !(searchDropdown.toLocaleLowerCase().includes(currentInput.toLocaleLowerCase()))){
             htmlDropdown.style.visibility = "hidden"}                                // Hide dropdown while input doesn't match spots
-    }
-
-        
+    }  
 })
 
